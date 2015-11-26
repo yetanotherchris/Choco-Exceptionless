83006101ea3d4e0823f697ed3c37daa2dfedbf9d
Chris Small <chris.small@totaljobsgroup.com>
2015-11-26 11:36:33 +0000

    Uninstall script now uses common.ps1

--- Exceptionless/tools/chocolateyuninstall.ps1
+++ Exceptionless/tools/chocolateyuninstall.ps1
@@ -1,32 +1,9 @@
-﻿Write-Host "Removing Exceptionless IIS site" -ForegroundColor Cyan
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. "$toolsDir\common.ps1"
 
 $appPoolName = "Exceptionless"
 $websiteName = "Exceptionless"
-$websitePort = 80
 
-Import-Module WebAdministration
-
-function Test-WebAppPool($Name) {
-    return Test-Path "IIS:\AppPools\$Name"
-}
-
-function Test-Website($Name) {
-    return Test-Path "IIS:\Sites\$Name"
-}
-
-#=================================================================
-# Remove existing app pool and site
-#=================================================================
-if (Test-WebAppPool $appPoolName)
-{
-    Write-Host "  Removing app pool $appPoolName"
-    Remove-WebAppPool -Name $appPoolName -WarningAction Ignore
-}
-
-if (Test-Website $websiteName)
-{
-    Write-Host "  Removing website $websiteName"
-    Remove-Website -Name $websiteName -WarningAction Ignore
-}
+Remove-Website $appPoolName $websiteName
 
 # The dependencies are left intact.
